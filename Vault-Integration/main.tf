df00c509b72beddf69c7a9be1b7c2bb9d3c5cd00
Yogesh <136956748+iam-yogeshreddy@users.noreply.github.com>
2025-01-30 18:46:44 +0530

    tf

---
+++ Vault-Integration/main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+provider "vault" {
+  address = "http://43.205.146.12:8200"
+  skip_child_token = true
+
+  auth_login {
+    path = "auth/approle/login"
+
+    parameters = {
+      role_id = "c5be6ef2-6ef3-5258-b815-f26252afe90e"
+      secret_id = "c90a2820-4641-2e50-024f-f7cd2e3005a9"
+    }
+  }
+}
+
+data "vault_kv_secret_v2" "example" {
+  mount = "kv" // change it according to your mount
+  name  = "test-secret" // change it according to your secret
+}
+
+resource "aws_instance" "my_instance" {
+  ami           = "ami-053b12d3152c0cc71"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "test"
+    Secret = data.vault_kv_secret_v2.example.data["username"]
+  }
+}

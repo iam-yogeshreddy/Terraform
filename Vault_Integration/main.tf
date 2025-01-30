3adf662eb87d4cec131a9cc753214450ba285247
Yogesh <136956748+iam-yogeshreddy@users.noreply.github.com>
2025-01-30 18:37:00 +0530

    tf

--- Vault_Integration/main.tf
+++ Vault_Integration/main.tf
@@ -4,21 +4,20 @@ provider "aws" {
 
 provider "vault" {
   address = "http://43.205.146.12:8200"
-  skip_child_token = true
 
   auth_login {
     path = "auth/approle/login"
 
     parameters = {
-      role_id = "c5be6ef2-6ef3-5258-b815-f26252afe90e"
+      role_id   = "c5be6ef2-6ef3-5258-b815-f26252afe90e"
       secret_id = "c90a2820-4641-2e50-024f-f7cd2e3005a9"
     }
   }
 }
 
 data "vault_kv_secret_v2" "example" {
-  mount = "kv" // change it according to your mount
-  name  = "test-secret" // change it according to your secret
+  mount = "kv"  # Change it according to your mount path
+  name  = "test-secret"  # Change it according to your secret
 }
 
 resource "aws_instance" "my_instance" {
@@ -26,7 +25,7 @@ resource "aws_instance" "my_instance" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "test"
+    Name   = "test"
     Secret = data.vault_kv_secret_v2.example.data["username"]
   }
 }

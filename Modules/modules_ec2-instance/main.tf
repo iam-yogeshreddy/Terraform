deee560844ec1aa8d0835328f8f46181927f2795
Yogesh <136956748+iam-yogeshreddy@users.noreply.github.com>
2025-01-30 23:30:47 +0530

    Update main.tf

--- Modules/modules_ec2-instance/main.tf
+++ Modules/modules_ec2-instance/main.tf
@@ -2,9 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "ec2_instance" {
-  source = "./modules/ec2_instance"
-  ami_value = "ami-053b12d3152c0cc71" # replace this
-  instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-0074fea0a1606239d". # replace this
+resource "aws_instance" "example" {
+    ami = var.ami_value
+    instance_type = var.instance_type_value
+    subnet_id = var.subnet_id_value
 }

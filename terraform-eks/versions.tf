81ca1374ecda65e1affbf88478a36393859bf78e
Yogesh <136956748+iam-yogeshreddy@users.noreply.github.com>
2025-07-05 17:09:11 +0530

    Update versions.tf

--- terraform-eks/versions.tf
+++ terraform-eks/versions.tf
@@ -1,17 +1,18 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 1.3.0"
+
   required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.5.1"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.46.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.32.0"
     }
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.46.0"
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
     }
     local = {
       source  = "hashicorp/local"
